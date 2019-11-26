33c55fedf834505150106021e66ce1dbfd4ed961
CHAO-YU HUANG <b608390@gmail.com>
2019-11-27 01:34:16 +0800

    Add files via upload

---
+++ parseJsonV3.py
@@ -0,0 +1,136 @@
+from __future__ import division
+
+from models import *
+from utils.utils import *
+from utils.datasets import *
+
+import os
+import sys
+import time
+import datetime
+import argparse
+
+from PIL import Image
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
+
+import json
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    #os.makedirs("output", exist_ok=True)
+    image_folder = 'data/test/'
+    model_def = 'config/yolov3-custom.cfg'
+    weights_path = 'checkpoints/yolov3_ckpt_40.pth'
+    class_path = 'data/custom/classes.names'
+    conf_thres = 0.8
+    nms_thres = 0.4
+    batch_size = 1
+    n_cpu = 0
+    img_size = 416
+    checkpoint_model = 'checkpoints/yolov3_ckpt_40.pth'
+
+    # Set up model
+    model = Darknet(model_def, img_size=img_size).to(device)
+
+    if weights_path.endswith(".weights"):
+        # Load darknet weights
+        model.load_darknet_weights(weights_path)
+    else:
+        # Load checkpoint weights
+        model.load_state_dict(torch.load(weights_path))
+
+    model.eval()  # Set in evaluation mode
+
+    dataloader = DataLoader(
+        ImageFolder(image_folder, img_size=img_size),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+    )
+
+    classes = load_classes(class_path)  # Extracts class labels from file
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    imgs = []  # Stores image paths
+    img_detections = []  # Stores detections for each image index
+
+    print("\nPerforming object detection:")
+    prev_time = time.time()
+    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
+        # Configure input
+        input_imgs = Variable(input_imgs.type(Tensor))
+        
+        # Get detections
+        with torch.no_grad():
+            detections = model(input_imgs)
+            detections = non_max_suppression(detections, conf_thres, nms_thres)
+
+        # Log progress
+        current_time = time.time()
+        inference_time = datetime.timedelta(seconds=current_time - prev_time)
+        prev_time = current_time
+        print("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+
+        # Save image and detections
+        imgs.extend(img_paths)
+        img_detections.extend(detections)
+
+    print("\nSaving images:")
+    result_list = []
+    # Iterate through images and save plot of detections
+    for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
+
+        print("(%d) Image: '%s'" % (img_i, path))
+
+        # Create plot
+        img = np.array(Image.open(path))
+        
+        result_boxes = []
+        result_score = []
+        result_label = []
+        # Draw bounding boxes and labels of detections
+        if detections is not None:
+            # Rescale boxes to original image
+            detections = rescale_boxes(detections, img_size, img.shape[:2])
+            unique_labels = detections[:, -1].cpu().unique()
+            n_cls_preds = len(unique_labels)
+            # bbox_colors = random.sample(colors, n_cls_preds)
+            for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+
+                #print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
+               
+                score = float(cls_conf.item())
+                if (score > 0.7):
+                    result_score.append(score)
+
+                    newBox = []
+                    newBox.append(int(y1))
+                    newBox.append(int(x1))
+                    newBox.append(int(y2))
+                    newBox.append(int(x2))
+                    result_boxes.append(newBox)
+
+                    label = int(classes[int(cls_pred)])
+                    if label == 0:
+                        label = 10
+                    result_label.append(label)
+
+        result_list.append({
+            'bbox': result_boxes,
+            'score': result_score,
+            'label': result_label
+        })
+        
+    with open('result.json', 'w') as outfile:
+        json.dump(result_list, outfile)
